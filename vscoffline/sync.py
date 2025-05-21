ba99907f6b965628c03990ac815a1f6a3cbcaa19
Moritz Grede <github.vrh9at@moritzgre.de>
2025-05-21 08:20:08 +0200

    Add default frequency back
    As per the feedback from the PR readd the default frequency back as the standard should not change.

--- vscoffline/sync.py
+++ vscoffline/sync.py
@@ -861,6 +861,8 @@ if __name__ == '__main__':
         config.updateextensions = True
         config.updatemalicious = True
         config.checkspecified = True
+        if not config.frequency:
+            config.frequency = '12h'
         config.existing = True
 
     if config.syncall:
